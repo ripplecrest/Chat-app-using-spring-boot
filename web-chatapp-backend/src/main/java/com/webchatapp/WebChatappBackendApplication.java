ff7b614878df3d17f5e63ef41ee3efb9c826fb3d
Shivang Sharma <2004shivang@gmail.com>
2024-12-17 01:25:43 +0530

    ruined everting fresh start

---
+++ web-chatapp-backend/src/main/java/com/webchatapp/WebChatappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.webchatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebChatappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebChatappBackendApplication.class, args);
+	}
+
+}
---
+++ web-chatapp-backend/src/test/java/com/webchatapp/WebChatappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webchatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebChatappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
